e90f4cec1274f9d831faffb3bcd0c947363f81c6
tiago-silva-git <tiago.silva.qa@gmail.com>
2022-08-29 20:03:24 -0300

    Consuming data from api

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,36 @@
 import React from "react";
+import axios from "axios";
+
+import { useState, useEffect } from "react";
+
 import SweetCard from "./SweetCard";
-import dataSweet from "../data/SweetList";
 import TotalSummary from "./TotalSummary";
 
 
 
 export default function Main() {
+    const [state, setState] = useState("");
+
+    useEffect(() => {
+        const request = async () => {
+            const response = await axios.get(
+                `https://chocolate-cart.vercel.app/acima-10-reais.json`
+              );
+   
+              const infosSweet = response.data.items;
+
+          setState(
+            infosSweet.map((elem, i) => <SweetCard image={elem.imageUrl} nameSweet={elem.name} fullPrice={`R$ ${elem.price}`} price={`R$ ${elem.sellingPrice}`} key={i}/>)
+          );
+
+        };
+        request();
+      }, [1]);
 
 
     return (
         <main>
-            {dataSweet.map((elem) => <SweetCard image={elem.image} fullPrice={elem.fullPrice} nameSweet={elem.nameSweet} price={elem.price}/>)}
+            {state}
             <TotalSummary />
         </main>
     );
